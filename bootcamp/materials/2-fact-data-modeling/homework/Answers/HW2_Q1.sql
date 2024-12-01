26eed5994119d1a2ce897ff8364979779d1c1385
Sebastian <sebastian.luehrsen@dnv.com>
2024-12-01 11:35:01 +0100

    changes

--- bootcamp/materials/2-fact-data-modeling/homework/Answers/HW2_Q1.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/Answers/HW2_Q1.sql
@@ -1,6 +1,12 @@
 -- Q1 | deduplicate game_details
+WITH 
+deduped AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER(partition by gd.game_id, gd.team_id, gd.player_id) as row_num
+    FROM game_details gd
+)
 SELECT
-    *,
-    ROW_NUMBER() OVER(partition by gd.game_id, gd.team_id, gd.player_id) as row_num
-FROM game_details gd
+    *
+FROM deduped
 WHERE row_num = 1
--- bootcamp/materials/2-fact-data-modeling/homework/Answers/HW2_WorkingDocument.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/Answers/HW2_WorkingDocument.sql
@@ -1,8 +1,14 @@
 -- Q1 | deduplicate game_details
+WITH 
+deduped AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER(partition by gd.game_id, gd.team_id, gd.player_id) as row_num
+    FROM game_details gd
+)
 SELECT
-    *,
-    ROW_NUMBER() OVER(partition by gd.game_id, gd.team_id, gd.player_id) as row_num
-FROM game_details gd
+    *
+FROM deduped
 WHERE row_num = 1
 
 -- Q2 | DDL for user_devices_cumulated
